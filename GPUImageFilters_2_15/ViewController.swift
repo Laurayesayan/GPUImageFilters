7a2e65e17bda5b9dead9b04069cc5a253ff53b55
laurayesayan <laurayesayan@gmail.com>
2020-05-05 16:27:45 +0300

    Initial Commit

---
+++ GPUImageFilters_2_15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPUImageFilters_2_15
+//
+//  Created by Лаура Есаян on 05.05.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
